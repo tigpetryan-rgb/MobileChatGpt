69d9d8bbf32ee7f7b859c4e29a407c4aeb58c6c5
tigpetryan-rgb <tigpetryan@gmail.com>
2026-09-02 14:16:07 +0400

    fix(android-test): remove invalid semantics collection import

--- android/app/src/androidTest/java/am/mobilechatgpt/RuntimeVerticalSliceTest.kt
+++ android/app/src/androidTest/java/am/mobilechatgpt/RuntimeVerticalSliceTest.kt
@@ -2,7 +2,6 @@ package am.mobilechatgpt
 
 import android.content.Context
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.fetchSemanticsNodes
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
